9fca2ef3a4eecdd855fb001cba81e6af9496448c
Eason Chen <me@easoncxz.com>
2013-07-25 12:21:37 +0000

    testCentsConstructor partially implemented.

--- src/se254/money/TestMoney.java
+++ src/se254/money/TestMoney.java
@@ -1,4 +1,5 @@
 package se254.money;
+
 /**
  * SOFTENG 254 2011 Assignment 1 submission
  *
@@ -7,22 +8,69 @@ package se254.money;
 
 import junit.framework.TestCase;
 
+/**
+ * As an assumption allowing for the logic of the tests, toString() is assumed
+ * to be implemented correctly (i.e. can reliably represent the state of a Money
+ * object correctly, as specified by the Money.toString() documentation.), due
+ * to it being the only public method of the Money class that can report the
+ * state of a Money object independently of the Money class.
+ * 
+ * @author (Xuzong Chen, xche985)
+ */
 public class TestMoney extends TestCase {
 
 	private Money money;
-	
-	public void setUp(){
+
+	public void setUp() {
 		money = new Money();
 	}
-	
-	public void testDefaultConstructor(){
-		assertTrue(money.equals(money));
+
+	/**
+	 * A trivial test.
+	 */
+	public void testDefaultConstructor() {
+		assertTrue(money != null);
+		assertEquals(money.toString(), "$0.00");
 	}
 
-    /**
-     * DO NOT DELETE THIS
-     * This is needed for the automatic marking process.
-     **/
+	/**
+	 * To test all cases for construction and toString().
+	 */
+	public void testCentsConstructor() {
+		Money m;
+		try {
+			m = new Money(0, 0);
+			assertEquals(m.toString(), "$0.00");
+			m = new Money(0, 99);
+			assertEquals(m.toString(), "$0.99");
+			m = new Money(0, -99);
+			assertEquals(m.toString(), "-$0.99");
+			m = new Money(1, 0);
+			assertEquals(m.toString(), "$1.00");
+			m = new Money(1, 10);
+			assertEquals(m.toString(), "$1.10");
+			try {
+				m = new Money(1, -10); // illegal argument
+			} catch (IllegalArgumentException e) {
+				// test passed.
+			}
+			m = new Money(-1, 0);
+			assertEquals(m.toString(), "-$1.00");
+			m = new Money(-1, 2);
+			assertEquals(m.toString(), "-$1.02");
+			try {
+				m = new Money(-1, -2); // illegal argument
+			} catch (IllegalArgumentException e) {
+				// test passed.
+			}
+		} catch (NullPointerException e) {
+			fail(); // i.e. for when m is null.
+		} // should capture Exception exceptions?
+	}
+
+	/**
+	 * DO NOT DELETE THIS This is needed for the automatic marking process.
+	 **/
 	// public static void main(String[] args) {
 	// junit.textui.TestRunner.run(TestInheritanceModel.class);
 	// }
